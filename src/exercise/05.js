0be660867647f710933c7446fc6c7c50e7df0170
William O'Connor <williamoconnor@williams-mbp-2.lan>
2022-06-30 07:10:57 -0500

    Complete all exercises

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,30 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = (
+  <div
+    style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+    className="box box--small"
+  >
+    small lightblue box
+  </div>
+)
+const mediumBox = (
+  <div
+    style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+    className="box box--medium"
+  >
+    medium pink box
+  </div>
+)
+const largeBox = (
+  <div
+    style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+    className="box box--large"
+  >
+    large orange box
+  </div>
+)
 
 function App() {
   return (
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,6 +5,11 @@ import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
   // 🐨 add a submit event handler here (`handleSubmit`).
+  const handleSubmit = event => {
+    event.preventDefault()
+    const username = event.target.elements.username.value
+    onSubmitUsername(username)
+  }
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
@@ -20,10 +25,10 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="username-input">Username:</label>
+        <input type="text" name="username" id="username-input" />
       </div>
       <button type="submit">Submit</button>
     </form>
